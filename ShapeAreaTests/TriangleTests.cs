1132749ffd0b067be98ca2276c56b49b631a1721
Mike <60842273+HonestShisha@users.noreply.github.com>
2023-07-30 07:20:22 +0300

    Добавил реализацию IShape для Triangle и тесты для Triangle

---
+++ ShapeAreaTests/TriangleTests.cs
@@ -0,0 +1,30 @@
+﻿using ShapesArea;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaTests {
+	public class TriangleTests {
+		[Theory()]
+		[InlineData(new[] { 10.0, 16.0, 9.0}, 40.907670429883929)]
+		[InlineData(new[] { 3.0, 4.0, 5.0}, 6.0)]
+		public void Triangle_area(double[] sidesLengths, double expected) {
+			var sut = new Triangle(sidesLengths);
+
+			var result = sut.Area();
+
+			Assert.Equal(expected, result);
+		}
+
+		[Theory()]
+		[InlineData(new[] { 10.0, 16.0, 9.0 }, false)]
+		[InlineData(new[] { 3.0, 4.0, 5.0 }, true)]
+		public void Can_distinguish_right_triangle(double[] sidesLengths, bool expected) {
+			var result = Triangle.IsRightTriangle(sidesLengths);
+
+			Assert.Equal(result, expected);
+		}
+	}
+}
--- ShapesArea/Triangle.cs
+++ ShapesArea/Triangle.cs
@@ -1,10 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ShapesArea {
+	public class Triangle : IShape {
+		public double[] SidesLengths { get; set; }
 
-namespace ShapesArea {
-	internal class Triangle {
+		public Triangle(double[] sidesLengths) {
+			if (sidesLengths.Length != 3) {
+				throw new Exception($"{nameof(sidesLengths)} должна содержать ровно три элемента.");
+			}
+
+			if (!IsValidTriangle(sidesLengths)) {
+				throw new Exception("Сумма длин сторон двух треугольника оказалась меньше длины третьей стороны.");
+			}
+
+			SidesLengths = sidesLengths;
+		}
+
+		public double Area() {
+			if (IsRightTriangle(SidesLengths)) {
+				return RightTriangleArea();
+			}
+			var a = SidesLengths[0];
+			var b = SidesLengths[1];
+			var c = SidesLengths[2];
+
+			double P = (a + b + c) / 2;
+			double area = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
+
+			return area;
+		}
+
+		public static bool IsRightTriangle(double[] sidesLengths) {
+			var a = sidesLengths[0];
+			var b = sidesLengths[1];
+			var c = sidesLengths[2];
+
+			var Lab = a * a + b * b;
+			var Lbc = b * b + c * c;
+			var Lca = c * c + a * a;
+
+			return Lab == c * c || Lbc == a * a || Lca == b * b;
+		}
+
+		private static bool IsValidTriangle(double[] sidesLengths) {
+			var a = sidesLengths[0];
+			var b = sidesLengths[1];
+			var c = sidesLengths[2];
+
+			return a + b > c && b + c > a && c + a > b;
+		}
+
+		private double RightTriangleArea() {
+			var sides = SidesLengths
+				.OrderBy(length => length)
+				.ToArray();
+			double a = sides[0];
+			double b = sides[1];
+
+			double area = a * b / 2;
+
+			return area;
+		}
 	}
 }
